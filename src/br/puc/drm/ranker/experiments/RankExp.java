33e74931012af9092b314abf2853f368f6a098ed
danielrosamarques <danielrosamarques@gmail.com>
2015-04-27 16:57:59 -0300

    Fixes various bugs

--- src/br/puc/drm/ranker/experiments/RankExp.java
+++ src/br/puc/drm/ranker/experiments/RankExp.java
@@ -15,134 +15,169 @@ import java.util.Scanner;
 import br.puc.drm.ranker.MetaRanker;
 import br.puc.drm.ranker.RankEvaluation;
 import weka.classifiers.Classifier;
+import weka.classifiers.bayes.NaiveBayes;
+import weka.classifiers.functions.MultilayerPerceptron;
+import weka.classifiers.functions.SMO;
+import weka.classifiers.lazy.KStar;
 import weka.classifiers.trees.J48;
+import weka.classifiers.trees.RandomForest;
 import weka.core.Instances;
 import weka.core.converters.ArffLoader;
 
 import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 
 public class RankExp {
 	
 	public static void main(String[] args) {
 		
-		//Command line parsing & testing		
+		//Messages		
 		final String USAGE = "Usage:java RankExp <Experiments File Path>";
-		final String MSG_TOO_FEW_ARGUMENTS  = "too few arguments";
-		final String MSG_TOO_MANY_ARGUMENTS = "too many arguments";
+		final String MSG_TOO_FEW_ARGUMENTS  = "Too few arguments";
+		final String MSG_TOO_MANY_ARGUMENTS = "Too many arguments";
+		final String MSG_INVALID_ARG = "Invalid argument";
+		final String MSG_INVALID_INPUT_FILE = "Invalid input file";
+		final String MSG_INVALID_JSON = "Invalid Json";
 		
+		System.out.println(USAGE);
+		
+		//Command line parsing & testing
 	    if (args.length < 1) {
 	    	
 	         System.out.println(MSG_TOO_FEW_ARGUMENTS);
+	         System.exit(0);
 	         
-	    } else if (args.length > 1)
+	    } else if (args.length > 1) {
 	    	
 	        System.out.println(MSG_TOO_MANY_ARGUMENTS);
-	    
-	    System.out.println(USAGE);
+	        System.exit(0);
+	        
+	    } else if (args[0] == null) {
+	    	
+	    	System.out.println(MSG_INVALID_ARG);
+	    	System.exit(0);
+	    }	    
         
 		//Auxiliary variables
 		List<File> files;
-		Classifier classifier;
 		String classifierOptions;
 		Integer numberOfFolds;
 		String outputResult = "";		
 		String inputFilePath = args[0];		
-		JSONObject jsonObj = new JSONObject(readInputFile(inputFilePath));
+		JSONObject jsonObj;
 		
-		if (jsonObj.has("experiments")) {
+		try {
 			
-			JSONArray experiments = jsonObj.getJSONArray("experiments");
+			jsonObj = new JSONObject(readInputFile(inputFilePath));
 			
-			for (int i = 0; i < experiments.length(); i++) {
+			if (jsonObj.has("experiments")) {
 				
-				JSONObject experiment = new JSONObject(experiments.get(i).toString());
+				JSONArray experiments = jsonObj.getJSONArray("experiments");
 				
-				System.out.println();
-				System.out.println(" # Experiment " + (i + 1));
-				System.out.println();
-				System.out.println(" - Json: " + experiment.toString());
+				for (int i = 0; i < experiments.length(); i++) {
+					
+					JSONObject experiment = new JSONObject(experiments.get(i).toString());
+					
+					System.out.println();
+					System.out.println(" # Experiment " + (i + 1));
+					System.out.println();
+					System.out.println(" - Json: " + experiment.toString());
 
-				if (experiment.has("directory") && experiment.has("classifier") && experiment.has("validation") ) {
-					
-					if (experiment.has("classifieroptions")) {
+					if (experiment.has("directory") && experiment.has("classifier") && experiment.has("validation") ) {
 						
-						classifierOptions = experiment.getString("classifieroptions");
+						if (experiment.has("classifieroptions")) {
+							
+							classifierOptions = experiment.getString("classifieroptions");
+							
+						} else {
+							
+							classifierOptions = null;
+						}					
+						
+						
+						if (experiment.has("numberoffolds")) {
+							
+							numberOfFolds = experiment.getInt("numberoffolds");
+							
+						} else {
+							
+							numberOfFolds = 10;
+						}
+						
+						files = listFilesForFolder(new File(experiment.getString("directory")));
+						
+						//Types of experiments
+						
+						//Metaranker cross validation
+						if (experiment.has("metaranker") && experiment.getBoolean("metaranker") == true 
+								&& experiment.getString("validation").equals("C")) {
+							
+							System.out.println(" - Metaranker Cross validation");
+							
+							for (File file : files) {
+								
+								Instances data = loadTestFile(file);
+								System.out.println(" - Dataset: " + file.getName());
+								
+								outputResult += experimentMetaCrossValidated(getClassifier(experiment.getString("classifier"), classifierOptions), data, numberOfFolds);
+							}						
+						}
+						
+						//Classifier cross validation
+						if (experiment.has("metaranker") && experiment.getBoolean("metaranker") == false 
+								&& experiment.getString("validation").equals("C")) {
+							
+							System.out.println(" - Classifier Cross Validation");
+							
+							for (File file : files) {
+								
+								Instances data = loadTestFile(file);			
+								
+								System.out.println(" - Dataset: " + file.getName());
+								
+								outputResult += experimentClassCrossValidated(getClassifier(experiment.getString("classifier"), classifierOptions), data, numberOfFolds);
+							}						
+						}
 						
 					} else {
 						
-						classifierOptions = null;
-					}					
-					
-					
-					if (experiment.has("numberoffolds")) {
-						
-						numberOfFolds = experiment.getInt("numberoffolds");
-						
-					} else {
-						
-						numberOfFolds = 10;
+						System.out.println(" - The experiment is missing parameters.");
 					}
-					
-					files = listFilesForFolder(new File(experiment.getString("directory")));
-					
-					//Types of experiments
-					
-					//Metaranker cross validation
-					if (experiment.has("metaranker") && experiment.getBoolean("metaranker") == true 
-							&& experiment.getString("validation").equals("C")) {
-						
-						System.out.println(" - Metaranker Cross validation");
-						
-						for (File file : files) {
-							
-							Instances data = loadTestFile("iris.arff");
-							System.out.println(" - Dataset: " + file.getName());
-							
-							outputResult += experimentMetaCrossValidated(getClassifier(experiment.getString("classifier"), classifierOptions), data, numberOfFolds);
-						}						
-					}
-					
-					//Classifier cross validation
-					if (experiment.has("metaranker") && experiment.getBoolean("metaranker") == false 
-							&& experiment.getString("validation").equals("C")) {
-						
-						System.out.println(" - Classifier Cross Validation");
-						
-						for (File file : files) {
-							
-							Instances data = loadTestFile("iris.arff");			
-							
-							System.out.println(" - Dataset: " + file.getName());
-							
-							outputResult += experimentClassCrossValidated(getClassifier(experiment.getString("classifier"), classifierOptions), data, numberOfFolds);
-						}						
-					}
-					
-				} else {
-					
-					System.out.println(" - The experiment is missing parameters.");
 				}
+				
+				//Write to output file
+				try {
+					
+					FileWriter writer = new FileWriter("Experiment_Results_" + System.currentTimeMillis() + ".csv");
+					writer.append("Score, Maximum_Score, Percentage, Elapsed_Time_Average\n");
+					writer.append(outputResult);
+					writer.flush();
+					writer.close();
+					
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				
+			} else { 
+				
+				System.out.println(" - No experiments to run."); 
+			
 			}
 			
-			//Write to output file
-			try {
-				
-				FileWriter writer = new FileWriter("Experiment_Results_" + System.currentTimeMillis() + ".csv");
-				writer.append("Score, Maximum_Score, Percentage, Elapsed_Time_Average\n");
-				writer.append(outputResult);
-				writer.flush();
-				writer.close();
-				
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-			
-		} else { 
-			
-			System.out.println(" - No experiments to run."); 
-		
+		} catch (FileNotFoundException e1) {
+
+	    	System.out.println(MSG_INVALID_INPUT_FILE);
+	    	System.exit(0);
+	    	
+		} catch (JSONException e2) {
+
+	    	System.out.println(MSG_INVALID_JSON);
+	    	System.exit(0);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
 	}
 	
@@ -176,34 +211,58 @@ public class RankExp {
 		
 	}
 
-	private static String readInputFile(String filePath) {
+	private static String readInputFile(String filePath) throws FileNotFoundException {
 		
 		String content;
+	
+		Scanner scanner = new Scanner(new File(filePath));
+		content = scanner.useDelimiter("\\Z").next();
+		scanner.close();
 		
-		try {
-			
-			content = new Scanner(new File(filePath)).useDelimiter("\\Z").next();
+		return content;
 
-			return content;
-			
-		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
-		return null;
 	}
 	//Instantiates a classifier
 	private static Classifier getClassifier(String classifierName, String classifierOptions) {
 		
-		J48 classifier = new J48();
+		Classifier classifier = null;
 		
 		try {
-			
-			if (classifierOptions != null) {
+		
+			switch (classifierName) {
+			case "J48":
+				classifier = new J48();
+				if (classifierOptions != null) ((J48) classifier).setOptions(weka.core.Utils.splitOptions(classifierOptions));
+				break;
+
+			case "NaiveBayes":
+				classifier = new NaiveBayes();
+				if (classifierOptions != null) ((NaiveBayes) classifier).setOptions(weka.core.Utils.splitOptions(classifierOptions));
+				break;
+
+			case "SMO":
+				classifier = new SMO();
+				if (classifierOptions != null) ((SMO) classifier).setOptions(weka.core.Utils.splitOptions(classifierOptions));
+				break;
 				
-				classifier.setOptions(weka.core.Utils.splitOptions(classifierOptions));
-			}
+			case "MultilayerPerceptron":
+				classifier = new MultilayerPerceptron();
+				if (classifierOptions != null) ((MultilayerPerceptron) classifier).setOptions(weka.core.Utils.splitOptions(classifierOptions));
+				break;
+			
+			case "KStar":
+				classifier = new KStar();
+				if (classifierOptions != null) ((KStar) classifier).setOptions(weka.core.Utils.splitOptions(classifierOptions));
+				break;
+			
+			case "RandomForest":
+				classifier = new RandomForest();
+				if (classifierOptions != null) ((RandomForest) classifier).setOptions(weka.core.Utils.splitOptions(classifierOptions));
+				break;
+				
+			default:
+				break;
+			}		
 						
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
@@ -214,7 +273,7 @@ public class RankExp {
 		
 	}
 	
-	private static Instances loadTestFile(String fileName) {
+	private static Instances loadTestFile(File file) {
 		
 		Instances data = null;
 		
@@ -222,7 +281,7 @@ public class RankExp {
 		
 	    try {
 	    	
-			loader.setFile(new File(fileName));
+			loader.setFile(file);
 			data = loader.getDataSet();
 			data.setClassIndex(data.numAttributes()-1);
 			
@@ -246,7 +305,7 @@ public class RankExp {
 	            
 	        } else {
 	        	
-	        	files.add(new File(folder.getAbsolutePath()+ "\\" + fileEntry.getName()));
+	        	files.add(new File(folder.getAbsolutePath()+ "/" + fileEntry.getName()));
 	        	
 	        }
 	    }
--- src/br/puc/drm/ranker/tests/RankExpTest.java
+++ src/br/puc/drm/ranker/tests/RankExpTest.java
@@ -1,13 +1,25 @@
 package br.puc.drm.ranker.tests;
 
+import static org.junit.Assert.*;
+
+import java.io.File;
+import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
 import org.junit.Test;
 
+import weka.classifiers.Classifier;
+import weka.classifiers.bayes.NaiveBayes;
+import weka.classifiers.functions.SMO;
+import weka.classifiers.trees.J48;
+import weka.core.Instances;
 import br.puc.drm.ranker.experiments.RankExp;
 
 public class RankExpTest {
 
 	@Test
-	public void test() {
+	public void mainTest() {
 		
 		String[] args = new String[1];
 		args[0] = "/home/daniel/workspace/RankerExp";
@@ -15,5 +27,86 @@ public class RankExpTest {
 		RankExp exp = new RankExp();
 		
 		exp.main(args);
+	}
+	
+	@Test
+	public void classifierShouldBeReturned() {
+		
+		RankExp exp = new RankExp();
+		Class[] cArgs = new Class[2];
+		cArgs[0] = String.class;
+		cArgs[1] = String.class;
+		try {
+			
+			Method method = exp.getClass().getDeclaredMethod("getClassifier", cArgs);
+			method.setAccessible(true);
+			
+			Classifier cls = (Classifier) method.invoke(exp, "J48", "-U");			
+			assertTrue(cls.getClass() == J48.class);
+
+			cls = (Classifier) method.invoke(exp, "J48", null);			
+			assertTrue(cls.getClass() == J48.class);
+
+			cls = (Classifier) method.invoke(exp, "NaiveBayes", "-K");			
+			assertTrue(cls.getClass() == NaiveBayes.class);
+			
+			cls = (Classifier) method.invoke(exp, "NaiveBayes", null);			
+			assertTrue(cls.getClass() == NaiveBayes.class);
+
+
+			cls = (Classifier) method.invoke(exp, "SMO", "-N 1 -M");			
+			assertTrue(cls.getClass() == SMO.class);
+			
+			cls = (Classifier) method.invoke(exp, "SMO", null);			
+			assertTrue(cls.getClass() == SMO.class);
+			
+		} catch (NoSuchMethodException | SecurityException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IllegalAccessException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IllegalArgumentException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (InvocationTargetException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	@Test
+	public void testFileShoulBeLoaded() {
+		
+		RankExp exp = new RankExp();
+		Class[] cArgs = new Class[1];
+		cArgs[0] = File.class;
+		
+		Method method;
+		try {
+			
+			method = exp.getClass().getDeclaredMethod("loadTestFile", cArgs);
+			method.setAccessible(true);
+			
+			File file = new File("/home/daniel/workspace/RankerExpFiles/iris.arff");
+			Instances data = (Instances) method.invoke(exp, file);			
+			assertFalse(data.isEmpty());
+			
+		} catch (NoSuchMethodException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SecurityException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IllegalAccessException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IllegalArgumentException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (InvocationTargetException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 }
